8ff11ef8d1707a0d5e466647241829921d038269
itsmiraz <mirajulislamofficial044@gmail.com>
2023-12-30 13:00:51 +0600

    new changes

---
+++ src/Context/UserContext.tsx
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+export const AUTHPROVIDER = createContext(undefined);
+
+const UserContext = () => {
+  return <div></div>;
+};
+
+export default UserContext;
